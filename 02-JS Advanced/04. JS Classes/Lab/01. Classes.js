6301bf9666f1cf5d970661eab5d2cff83cff0f6b
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-15 12:02:44 +0300

    Add files via upload

---
+++ 02-JS Advanced/04. JS Classes/Lab/01. Classes.js
@@ -0,0 +1,15 @@
+class Rectangle {
+    constructor(width, height, color) {
+        this.width = width;
+        this.height = height;
+        this.color = color;
+    }
+
+    calcArea() {
+        return this.width * this.height;
+    }
+}
+
+const rectangle = new Rectangle(10, 10, 'red');
+
+console.log(Rectangle.prototype);
