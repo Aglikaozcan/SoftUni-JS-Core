568361bd28523c31c9b39415eb00617e1d99b39c
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-15 12:38:36 +0300

    Add files via upload

---
+++ 03-JS Apps/04. ROUTING AND ARCHITECTURE/Team-Manager-Skeleton/helpers/storage.js
@@ -0,0 +1,16 @@
+const storage = function() {
+    const appKey = '';
+    const appSecret = '';
+
+    const getData = function() {
+
+    };
+
+    const saveData = function(key, value) {
+        localStorage.setItem(key + appKey, )
+    };
+
+    return {
+
+    };
+};
