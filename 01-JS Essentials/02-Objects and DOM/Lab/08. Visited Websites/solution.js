8cbceb5557e4cb6fdf048b5773573334cb6db265
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-15 11:30:07 +0300

    Add files via upload

--- 01-JS Essentials/02-Objects and DOM/Lab/08. Visited Websites/solution.js
+++ 01-JS Essentials/02-Objects and DOM/Lab/08. Visited Websites/solution.js
@@ -1,1 +1,19 @@
+function solve() {
+  let siteElements = document.getElementsByTagName('a');  
 
+  for (let i = 0; i < siteElements.length; i++) {
+
+    let site = siteElements[i];
+
+    site.addEventListener('click', function () {
+      let counterElement = document.getElementsByTagName('p')[i];
+      let counterText = counterElement.textContent;
+
+      let siteInfo = counterText.split(' ');      
+
+      siteInfo = siteInfo[0] + ' ' + (+siteInfo[1] + 1) + ' ' + siteInfo[2];
+
+      counterElement.textContent = siteInfo;
+    })
+  }
+}
