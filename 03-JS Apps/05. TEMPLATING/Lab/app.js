b28d81f9165c6c5d9b2abd7f08579b195e24e511
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-15 12:43:39 +0300

    Add files via upload

--- 03-JS Apps/05. TEMPLATING/Lab/app.js
+++ 03-JS Apps/05. TEMPLATING/Lab/app.js
@@ -1,1 +1,19 @@
+(async function () {
+    const { getTemplateFunc, getTemplateString } = window.templates;
 
+    const cardTemplate = await getTemplateString('card');
+    Handlebars.registerPartial('card', cardTemplate);
+
+    const cardsListFunc = await getTemplateFunc('cards');
+
+    document.getElementById('contacts').innerHTML = cardsListFunc({ contacts });
+
+    [...document.getElementsByClassName('detailsBtn')]
+        .forEach(btn => {
+            btn.addEventListener('click', handleDetails);
+        });
+
+    const handleDetails = (event) => {
+        console.log(event.target);
+    };
+})(); 
