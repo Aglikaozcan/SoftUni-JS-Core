37f6a5593cd1e02b15001405a5c32ca790ba97b7
Aglika Ozcan <39984578+Aglikaozcan@users.noreply.github.com>
2020-07-15 11:27:41 +0300

    Add files via upload

--- 01-JS Essentials/02-Objects and DOM/Lab/05. Articles List/solution.js
+++ 01-JS Essentials/02-Objects and DOM/Lab/05. Articles List/solution.js
@@ -1,1 +1,25 @@
+function createArticle() {
+	//първо взимаме нужните полета
+	let titleElement = document.getElementById('createTitle').value;
+	let textElement = document.getElementById('createContent').value;
+	let articleList = document.getElementById('articles');
 
+	if (titleElement && textElement) {
+		//създаваме новите полета
+		let articleElement = document.createElement('article');
+
+		let articleTitle = document.createElement('h3');
+		articleTitle.textContent = titleElement;
+
+		let articleText = document.createElement('p');
+		articleText.textContent = textElement;
+
+		//вкарваме ги по местата им
+		articleElement.appendChild(articleTitle);
+		articleElement.appendChild(articleText);
+		articleList.appendChild(articleElement);
+	}
+	//накрая трябва да изтрием съдържанието в полетата
+	document.getElementById('createTitle').value = "";
+	document.getElementById('createContent').value = "";
+}
